e17682efadc0d61dced2f68bb39b525bcb5fdf1a
ClaudioJRD <163933841+ClaudioJRD@users.noreply.github.com>
2024-03-19 20:29:15 -0300

    Pokedex

---
+++ Pokedex/js/scripts.js
@@ -0,0 +1,60 @@
+
+const pokemons = []
+
+function carregarPokemons(){
+
+    const pokemon1 = 
+    {
+        id: 1,
+        nome: "Bulbasaur",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+
+    }
+
+    pokemons.push(pokemon1)
+
+    const pokemon2 =
+    {
+        id: 2,
+        nome: "squirtle",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
+    }
+
+    pokemons.push(pokemon2)
+    
+    const pokemon3 =    
+    {
+        id: 3,
+        nome: "charmander",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
+
+    }
+    pokemons.push(pokemon3)
+
+    const pokemon4 =
+    {
+        id: 4,
+        nome: "weezing",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/110.png"
+    }
+    pokemons.push(pokemon4)
+
+    const pokemon5 = 
+    {
+        id: 5,
+        nome:"magikarp",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/129.png"
+    }
+    pokemons.push(pokemon5)
+
+     
+    const pokemon6 =
+    {
+        id: 6,
+        nome: "arcanine",
+        imagem: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/59.png"
+    }
+    pokemons.push(pokemon6)
+
+
+carregarPokemons()
