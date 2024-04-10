5a962d617885838d9c19cdbc4433206de1800d3f
ClaudioJRD <163933841+ClaudioJRD@users.noreply.github.com>
2024-04-09 21:40:06 -0300

    Lista de Pokemon
    aula 09/04/2024

---
+++ Aula 05 - Projeto PokeDex/js/scripts.js
@@ -0,0 +1,59 @@
+
+
+const grupoGrama = ["bulbasaur","ivysaur","venusaur"]
+const grupoFogo = ["charmander", "charmeleon", "charizard"]
+const grupoAgua = ["squirtle", "wartortle", "blastoise"]
+const grupoTerra = ["rattata", "raticate"]
+const grupoInseto = ["caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill"]
+const grupoVoador = ["pidgey", "pidgeotto", "pidgeot"]
+
+function carregarPokemons(){
+
+
+
+    const requisicaoHttp = new XMLHttpRequest()
+    requisicaoHttp.open("GET","https://pokeapi.co/api/v2/pokemon",false)
+    requisicaoHttp.send()
+
+    const resposta = JSON.parse(requisicaoHttp.responseText)
+    const pokemons = resposta.results
+
+    for( let index = 0 ; index < pokemons.length; index ++){
+        const pokemon = pokemons[index]
+        const id = index + 1
+        pokemon.imagem = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + id +".png"
+        adicionarCardPokemon(pokemon)
+    }
+}
+
+
+function adicionarCardPokemon(pokemon){
+
+    console.log(pokemon)
+    const imagemElemento = document.createElement("img")
+    imagemElemento.setAttribute("src", pokemon.imagem)
+
+    const textoElemento = document.createElement("h1")
+    textoElemento.innerHTML = pokemon.name
+    if(grupoGrama.includes(pokemon.name)){
+        textoElemento.classList.add("grama")
+    } else if(grupoFogo.includes(pokemon.name)){
+        textoElemento.classList.add("fogo")
+    } else if(grupoAgua.includes(pokemon.name)){
+        textoElemento.classList.add("agua")
+    } else if(grupoInseto.includes(pokemon.name)){
+        textoElemento.classList.add("inseto")
+    } else if(grupoTerra.includes(pokemon.name)){
+        textoElemento.classList.add("terra")
+    } else if(grupoVoador.includes(pokemon.name)){
+        textoElemento.classList.add("voador")
+    }
+
+
+    const divElemento = document.getElementById("conteudo-pokemon")
+    divElemento.classList.add("card")
+    divElemento.appendChild(imagemElemento)
+    divElemento.appendChild(textoElemento)
+    
+
+}
